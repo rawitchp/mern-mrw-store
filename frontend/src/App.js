3987f3f6e5a54baae46f6a5efd42c7a68c6b35f0
Rawitch <mrw@MacBook-Air-khxng-Rawitch.local>
2022-10-12 17:51:51 +0700

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">MRW</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
