ef7e37a630b6618ba1c74f967b768d1673149f8f
Rawitch <mrw@MacBook-Air-khxng-Rawitch.local>
2022-10-13 00:48:52 +0700

    create server.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './pages/HomeScreen';
+import ProductScreen from './pages/ProductScreen';
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">MRW</a>
-      </header>
-      <main>list products</main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">MRW</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
