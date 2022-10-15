aec13881f092ec00dceca1890beec268f39c9b3e
Rawitch <mrw@MacBook-Air-khxng-Rawitch.local>
2022-10-15 16:27:35 +0700

    Add Signin Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { Badge, Nav } from 'react-bootstrap';
 import { Store } from './Store';
 import CartScreen from './pages/CartScreen';
+import SigninScreen from './pages/SigninScreen';
 
 function App() {
   const { state } = useContext(Store);
@@ -41,6 +42,7 @@ function App() {
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/" element={<HomeScreen />} />
               <Route path="/cart" element={<CartScreen />} />
+              <Route path="/signin" element={<SigninScreen />} />
             </Routes>
           </Container>
         </main>
