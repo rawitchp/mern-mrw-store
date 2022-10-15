c4c6dddca4369c25f28e0c00ec8f367f030d097c
Rawitch <mrw@MacBook-Air-khxng-Rawitch.local>
2022-10-15 16:55:54 +0700

    connect DB

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,16 @@
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('connected to DB');
+  })
+  .catch((err) => console.log(err.message));
 
 const app = express();
 
@@ -17,13 +28,13 @@ app.get('/api/products/slug/:slug', (req, res) => {
 });
 
 app.get('/api/products/:id', (req, res) => {
-    const product = data.products.find((x) => x._id === req.params.id);
-    if (product) {
-      res.send(product);
-    } else {
-      res.status(404).send({ message: 'Product Not Found' });
-    }
-  });
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
