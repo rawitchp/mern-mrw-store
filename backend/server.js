6e26c4a72a20bc970e1241e1d7415e8a5975c9a4
Rawitch <mrw@MacBook-Air-khxng-Rawitch.local>
2022-10-16 00:58:46 +0700

    create Sign-in API

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -15,10 +16,18 @@ mongoose
   .catch((err) => console.log(err.message));
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/seed', seedRouter);
 //test
-app.use('/api/products',productRouter)
+app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
 
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
