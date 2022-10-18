88be721ce9fdd16140f0a7ea4a32b5a76bdd5d32
Rawitch <mrw@MacBook-Air-khxng-Rawitch.local>
2022-10-18 17:17:41 +0700

    create Profile Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,7 @@ import PaymentMethodScreen from './pages/PaymentMethodScreen';
 import PlaceOrderScreen from './pages/PlaceOrderScreen';
 import OrderScreen from './pages/OrderScreen';
 import OrderHistoryScreen from './pages/OrderHistoryScreen';
+import ProfileScreen from './pages/ProfileScreen';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -28,6 +29,7 @@ function App() {
     localStorage.removeItem('userInfo');
     localStorage.removeItem('shippingAddress');
     localStorage.removeItem('paymentMethod');
+    window.location.href = '/signin';
   };
   return (
     <BrowserRouter>
@@ -90,6 +92,7 @@ function App() {
               <Route path="/placeorder" element={<PlaceOrderScreen />} />
               <Route path="/order/:id" element={<OrderScreen />} />
               <Route path="/orderhistory" element={<OrderHistoryScreen />} />
+              <Route path="/profile" element={<ProfileScreen />} />
             </Routes>
           </Container>
         </main>
